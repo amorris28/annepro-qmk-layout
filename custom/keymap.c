5a5f80a875eed0dcd8fdf3487e0058b6403a1ec3
Andrew Morris <amorris3@uoregon.edu>
2022-06-23 14:37:13 -0700

    fix some bugs and turn on LEDs

--- custom/keymap.c
+++ custom/keymap.c
@@ -42,7 +42,7 @@ enum anne_pro_layers {
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
     LT(_FN1_LAYER,KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
-    KC_LCTL, KC_LGUI, KC_FN1_LAYER, KC_SPC, LT(_FN2_LAYER,KC_LEFT), LT(_LALT,KC_DOWN), LT(_RALT,KC_UP), RCTL_T(KC_RGHT)
+    KC_LCTL, KC_LGUI, MO(_FN1_LAYER), KC_SPC, LT(_FN2_LAYER,KC_LEFT), LALT_T(KC_DOWN), RALT_T(KC_UP), RCTL_T(KC_RGHT)
 ),
   /*
   * Layer _FN1_LAYER
@@ -62,9 +62,9 @@ enum anne_pro_layers {
  [_FN1_LAYER] = KEYMAP( /* Base */
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NO, KC_NO, KC_DEL,
-    MO(_FN2_LAYER), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_NO, KC_TRNS,
+    MO(_FN2_LAYER), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,
     KC_CAPS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_VOLU, KC_VOLD, KC_MPLY
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
   /*
   * Layer _FN2_LAYER
@@ -86,7 +86,7 @@ enum anne_pro_layers {
     KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, KC_NO, KC_DEL,
     KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_TRNS,
     KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT, KC_VOLU, KC_VOLD, KC_MPLY
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
  ),
 };
 const uint16_t keymaps_size = sizeof(keymaps);
@@ -104,11 +104,11 @@ void keyboard_post_init_user(void) {
     // Here are two common functions that you can use. For more LED functions, refer to the file "qmk_ap2_led.h"
 
     // annepro2-shine disables LEDs by default. Uncomment this function to enable them at startup.
-    // annepro2LedEnable();
+    annepro2LedEnable();
 
     // Additionally, it also chooses the first LED profile by default. Refer to the "profiles" array in main.c in
     // annepro2-shine to see the order. Replace "i" with the index of your preferred profile. (i.e the RED profile is index 0)
-    // annepro2LedSetProfile(i);
+    annepro2LedSetProfile(0);
 }
 
 layer_state_t layer_state_set_user(layer_state_t layer) {
